4647125b8a5ee61267368884cf3748ffa064531a
rimonspi12 <rimonmpi@gmail.com>
2021-11-02 23:10:50 +0600

    navigation part is complete

---
+++ src/Pages/Home/AppointmentBanner/AppointmentBanner.js
@@ -0,0 +1,46 @@
+import React from 'react';
+import Box from '@mui/material/Box';
+import Grid from '@mui/material/Grid';
+import doctor from '../../../images/doctor.png';
+import bg from '../../../images/appointment-bg.png';
+import { Button, Typography } from '@mui/material';
+
+const appointmentBanner = {
+    background: `url(${bg})`,
+    backgroundColor: 'rgba(45,58,74,.9)',
+    backgroundBlendMode:'darken, luminosity',
+    marginTop: 175
+
+}
+
+const AppointmentBanner = () => {
+    return (
+        <Box style={appointmentBanner} sx={{ flexGrow: 1 }}>
+            <Grid container spacing={2}>
+                <Grid item xs={12} md={6}>
+                    <img
+                        style={{ width: 400, marginTop: '-110px' }}
+                        src={doctor} alt="" />
+                </Grid>
+                <Grid item xs={12} md={6} sx={{ display: 'flex',justifyContent:'flex-start',alignItems:'center',textAlign:'left' }}>
+                  <Box>
+                        <Typography variant="h6" sx={{mb:5}} style={{ color: '#5CE7ED' }}>
+                            Appointment
+                        </Typography>
+
+                        <Typography variant="h4" sx={{ my: 5 }} style={{ color: 'white' }}>
+                            Make an Appointment Today
+                        </Typography>
+
+                        <Typography variant="h6" style={{ color: 'white', fontSize: '14px', fontWeight: '400' }}>
+                            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ea sapiente quae eaque aliquid atque officia blanditiis accusamus, quis ab fuga.
+                        </Typography>
+                        <Button variant="contained" style={{ backgroundColor:'#5CE7ED'}}>learn more</Button>
+                  </Box>
+                </Grid>
+            </Grid>
+        </Box>
+    );
+};
+
+export default AppointmentBanner;
--- src/Pages/Home/Home/Home.js
+++ src/Pages/Home/Home/Home.js
@@ -1,14 +1,15 @@
 import React from 'react';
 import Navigation from '../../Shared/Navigation/Navigation';
 import Services from '../Services/Services';
+import AppointmentBanner from '../AppointmentBanner/AppointmentBanner';
 
 const Home = () => {
     return (
         <div>
            <Navigation></Navigation>
            <Services></Services>
+           <AppointmentBanner></AppointmentBanner>
         </div>
     );
 };
-
 export default Home;
