dd2d012824bbab306add351eb9d68263e531f390
rimonspi12 <rimonmpi@gmail.com>
2021-11-02 16:40:30 +0600

    doctor client project

---
+++ src/Pages/Home/Services/Services.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import { experimentalStyled as styled } from '@mui/material/styles';
+import Box from '@mui/material/Box';
+import Grid from '@mui/material/Grid';
+import { Container } from '@mui/material';
+import Service from '../Service/Service';
+
+
+const Services = () => {
+    return (
+        <Box sx={{ flexGrow: 1 }}>
+            <Container>
+                <Grid container spacing={{ xs: 2, md: 3 }} columns={{ xs: 4, sm: 8, md: 12 }}>
+                    {Array.from(Array(6)).map((_, index) => (
+                        <Grid item xs={4} sm={4} md={4} key={index}>
+                            <Service></Service>
+                        </Grid>
+                    ))}
+                </Grid>
+            </Container>
+        </Box>
+    );
+};
+
+export default Services;
